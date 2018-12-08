43b4163ee24c05a0ca4d490e4527847e94bb0545
chenmeilin <51003960@qq.com>
2018-12-08 15:50:50 +0800

    第一次提交

---
+++ src/components/three.vue
@@ -0,0 +1,27 @@
+<template>
+ <div class="three" :style="{backgroundColor:beijing}">第三个路由
+      <br>
+   <button @click="bianse">点我变色</button>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'three',
+  data () {
+    return {
+        beijing:"black"
+    //   msg: 'Welcome to Your Vue.js App'
+    }
+  },
+    methods:{
+    bianse(){
+     this.beijing='greenyellow'
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
---
+++ src/components/two.vue
@@ -0,0 +1,27 @@
+<template>
+ <div class="two" :style="{backgroundColor:beijing}">第二个路由
+     <br>
+   <button @click="bianse">点我变色</button>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'two',
+  data () {
+    return {
+        beijing:"green"
+        // msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods:{
+    bianse(){
+     this.beijing='yellow'
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
